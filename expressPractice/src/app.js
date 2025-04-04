efa00afbc4a24cd63edca02d4dca83a21f4f072a
juanB <juancruzberrios73@gmail.com>
2025-04-04 17:26:17 -0300

    cambio de nombre carpetas

--- expressPractice/src/app.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const startServer=require('./server/server.js')
-
-const envs ={
-    port: process.env.PORT,
-    //publicPath: get("PUBLIC_PATH").default('public').asString(),
-};
-
-startServer(envs);
